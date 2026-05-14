e9ae0cdf4eaa06fbe3a509f1bde6d1a67efa0624
Lewis2002-William <lws2002mail@gmail.com>
2026-05-14 20:03:05 +0800

    Open Douyin POI after comment copy

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 window.PAGE_CONFIG = {
-  gymName: "顶尖健身房",
+  gymName: "燃点健身房",
   wifiSsid: "GYM-GUEST",
   wifiPassword: "Move2026",
   entryUrl: "",
+  douyinPoiUrl: "snssdk1128://poi/detail/?id=7626216596538198050",
   commentsPerView: 6,
   rotationHours: 6
 };
