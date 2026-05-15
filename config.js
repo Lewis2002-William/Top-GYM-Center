15a27db7054cfca19e9ea444268f9efd9487fed2
Lewis2002-William <lws2002mail@gmail.com>
2026-05-15 17:40:12 +0800

    config: 更新 WiFi 名称和密码

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.PAGE_CONFIG = {
   gymName: "燃点健身房",
-  wifiSsid: "GYM-GUEST",
-  wifiPassword: "Move2026",
+  wifiSsid: "TOPGYM_5G",
+  wifiPassword: "topgym666888",
   entryUrl: "",
   douyinPoiUrl: "snssdk1128://poi/detail/?id=7626216596538198050",
   commentsPerView: 6,
